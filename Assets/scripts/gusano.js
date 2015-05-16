8bd81430eeb8da434ea291c5db8cb5aa464af9f9
Elias <eliasdefrancisco@gmail.com>
2015-05-16 20:09:15 +0200

    Terreno integrado
    Signed-off-by: Elias <eliasdefrancisco@gmail.com>

--- Assets/scripts/gusano.js
+++ Assets/scripts/gusano.js
@@ -3,8 +3,8 @@
 var terrenoLocal: GameObject;
 var cuerposLocal: GameObject;
 var tiempoMueve: float = 0.2;
-var posX: int = 2;
-var posZ: int = 2;
+var posX: int = 10;
+var posZ: int = 3;
 var maxPosX: int = 0;
 var maxPosZ: int = 0;
 var cuerposLocalTransform: Transform;
@@ -15,18 +15,22 @@ function Start () {
 	terrenoLocalTransform = terrenoLocal.transform;
 	maxPosX = terrenoLocal.GetComponent(terreno).ancho;
 	maxPosZ  = terrenoLocal.GetComponent(terreno).alto;
-	var pos = terrenoLocalTransform.GetChild(posX).transform.GetChild(posZ).position;
-	pos.y = 1;
+	/*var pos = terrenoLocalTransform.GetChild(posX).transform.GetChild(posZ).position;
+	pos.y = 1;*/
+	var pos = getPosTablero();
 	this.transform.position = pos;
 	Invoke('mueveWorm', tiempoMueve);
 	
+	for(var child: Transform in terrenoLocalTransform){
+		Debug.Log(child.name);
+	}
 }
 
 function mueveWorm(){
 
 	// Direccion del movimiento
 	var direcc = this.GetComponent(controles).direcc;
-	if(direcc=='arr' && posZ < maxPosZ){	
+	if(direcc=='arr' && posZ < maxPosZ-1){	
 		posZ++; 
 	}
 	else if(direcc=='abj' && posZ > 0){	
@@ -35,13 +39,14 @@ function mueveWorm(){
 	else if(direcc=='izq' && posX > 0){	
 		posX--; 
 	}
-	else if(direcc=='der' && posX < maxPosX){	
+	else if(direcc=='der' && posX < maxPosX-1){	
 		posX++; 
 	}
 	
 	// Extrae la nueva posicion desde las baldosas y posiciona
-	var pos = terrenoLocalTransform.GetChild(posX).transform.GetChild(posZ).position;
-	pos.y = 1;
+	/*var pos = terrenoLocalTransform.GetChild(posX).transform.GetChild(posZ).position;
+	pos.y = 1;*/
+	var pos = getPosTablero();
 	var posAnt = this.transform.position;
 	this.transform.position = pos;
 	
@@ -54,6 +59,11 @@ function mueveWorm(){
 	Invoke('mueveWorm', tiempoMueve);
 }
 
+function getPosTablero(){
+	var pos = terrenoLocalTransform.GetChild(posX).transform.GetChild(posZ).position;
+	pos.y = 1;
+	return pos;
+}
 
 function Update () {
  
